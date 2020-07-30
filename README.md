fe0aa01b8e9dd4a0f0bd0d9da8b078c1253747f3
Lucas V. Schuermann <lucas.v.schuermann@gmail.com>
2020-07-30 19:49:38 -0400

    cleanup

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 My personal environment configuration files
 
 ## Usage
-Pull the repository, then create the symbolic links using GNU stow.
+Pull the repository, optionally install package dependencies `./install_deps.sh`, then create the symbolic links using GNU stow.
 ```
 $ git clone git@github.com:cerrno/dotfiles.git ~/.dotfiles
 $ cd ~/.dotfiles
--- install_deps.sh
+++ install_deps.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 if [ "$(uname)" != "Darwin" ]; then
     echo "This script has not been tested on non-Darwin environments. Exiting."
     exit 1
@@ -24,13 +22,3 @@ $(brew --prefix)/opt/fzf/install
 # set fish as default shell
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
-
-mkdir -p ~/.config
-
-ln -s ${BASEDIR}/kitty ~/.config/kitty
-ln -s ${BASEDIR}/fish ~/.config/fish
-ln -s ${BASEDIR}/nvim ~/.config/nvim
-
-# git
-ln -s ${BASEDIR}/gitconfig ~/.gitconfig
-ln -s ${BASEDIR}/gitignore_global ~/.gitignore_global
